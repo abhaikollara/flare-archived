9018c655575526fdab39be9ce1581ddd06350688
Abhai Kollara Dilip <abhai.dilip@gmail.com>
2018-01-09 20:56:41 +0530

    Basic train() works

--- trainer.py
+++ trainer.py
@@ -3,13 +3,19 @@ from torch.autograd import Variable
 from torch import optim
 from torch.utils.data import Dataset, DataLoader
 import numpy as np
+from tqdm import tqdm
+
+
 
 def _wrap_in_tensor(array):
 	pass
 
 
-def _wrap_in_variable(arrays):
-	pass
+def _wrap_in_variable(tensors):
+	if isinstance(tensors, (list, tuple)):
+		return [Variable(x) for x in tensors]
+	else:
+		return Variable(tensors)
 
 
 def _get_optimizer(optimizer, model):
@@ -34,6 +40,22 @@ def _get_optimizer(optimizer, model):
 			raise TypeError('Optimizer not understood')
 
 
+class dataset(Dataset):
+	
+	def __init__(self, inputs, targets):
+		super(dataset, self).__init__()
+		self.inputs = inputs
+		self.targets = targets
+
+	def __len__(self):
+		return self.inputs.size()[0]
+
+	def __getitem__(self, idx):
+		if not isinstance(self.inputs, (list, tuple)):
+			return self.inputs[idx], self.targets[idx]
+		else:
+			return [x[idx] for x in self.inputs], self.targets[idx]
+
 
 class Trainer(object):
 
@@ -44,9 +66,29 @@ class Trainer(object):
 
 
 	def train(self, inputs, targets, batch_size=None, epochs=1, validation_split=0.0, validation_data=None, shuffle=True, class_weight=None):
-		pass
+		'''
+			TODO : 
+			1. Validation data
+			2. Shuffle
+			3. Class weight
+		'''
+		if validation_data:
+			train_dataset = dataset(inputs, targets)
+			valid_dataset = None # TODO
+		elif validation_split > 0.0:
+			split_size = int(len(inputs) * validation_split)
+			train_dataset = dataset(inputs[:-split_size], targets[:-split_size])
+			valid_dataset = dataset(inputs[:-split_size], targets[:-split_size])
+		else:
+			train_dataset = dataset(inputs, targets)
 
+		train_data_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=shuffle)
 
+		for epoch in range(epochs):
+			print("Epoch", str(epoch), "of", str(epochs))
+			for batch in tqdm(train_data_loader):
+				batch_inputs, batch_targets = batch
+				self.train_batch(batch_inputs, batch_targets)
 
 
 	def evaluate(self, inputs, targets, batch_size=None, metrics=['accuracy']):
@@ -60,10 +102,11 @@ class Trainer(object):
 	def train_batch(self, inputs, targets, class_weight=None):
 
 		self.optimizer.zero_grad()
-
+		input_batch = _wrap_in_variable(inputs)
+		target_batch = _wrap_in_variable(targets)
 		# TODO : Make inputs and targets accept np nd-arrayss
-		y = self.model(inputs)
-		loss = self.loss_func(y, targets)
+		y = self.model(input_batch)
+		loss = self.loss_func(y, target_batch)
 		loss.backward()
 		self.optimizer.step()
 
